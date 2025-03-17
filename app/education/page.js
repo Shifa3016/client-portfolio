e8d5153cc79a816a6ab60e2b75b86e546a2ead42
Shifa3016 <shifam.ug23.cs@nitp.ac.in>
2025-03-17 23:23:35 +0530

    Update margin

--- app/education/page.js
+++ app/education/page.js
@@ -11,7 +11,7 @@ const page = () => {
                     <div>
                         <Heading title="Academic Background" />
                     </div>
-                    <div className="mx-8">
+                    <div className="mx-2">
                         <Details title="Ph. D" detail="Indian Institute of Information Technology, Design & Manufacturing, Jabalpur | 2019-2023 | 8.4 CGPA" />
                         <Details title="M. Tech." detail="Indian Institute of Information Technology, Design & Manufacturing, Jabalpur | 2019 | 7.80 CGPA" />
                         <Details title="B.E.(ME)" detail="P.E.S College of Engineering | Dr. BAMU, Aurangabad | 2016 | 71.70" />
@@ -22,7 +22,7 @@ const page = () => {
 
                 <div className="trainings my-4">
                     <Heading title="Certified Courses" />
-                    <div className="mx-8">
+                    <div className="mx-2">
                         <Details title="Teaching and Learning in general Programs: TALG" detail="NPTEL (SWAYAM course) | 4 Week course (Jul-Aug 2022)" />
                         <Details title="Effective Engineering Teaching in Practice" detail="NPTEL (SWAYAM course) | 4 Week course (Jan-Feb 2021)" />
                         <Details title="Ansys Workbench" detail="EICT academy, IIITDM Jabalpur | 5 days (22nd Jan-26th Jan’18)" />
@@ -36,7 +36,7 @@ const page = () => {
 
                 <div className="skills my-4">
                     <Heading title="Technical Skills" />
-                    <div className="mx-8">
+                    <div className="mx-2">
                         <Details title="Modeling Software" detail="SolidWorks | Fusion-360 | AutoCAD" />
                         <Details title="Simulation Software" detail="Ansys CAE" />
                         <Details title="3D Printing Software" detail="Flash Print | Cura | Repetier host" />
