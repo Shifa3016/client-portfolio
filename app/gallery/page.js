6885596d907d5465307c9ae0e3fae227adda1e48
Shifa3016 <shifam.ug23.cs@nitp.ac.in>
2025-03-17 19:58:12 +0530

    Gallery page uploaded

---
+++ app/gallery/page.js
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Heading from "../components/Heading";
+
+export default function AftabGallery() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/aftab")
+      .then((res) => res.json())
+      .then((data) => setImages(data))
+      .catch((err) => console.error("Error fetching images:", err));
+  }, []);
+
+  return (
+    <div className="pt-[56px]">
+        <div className="mx-8 my-3">
+      <Heading title="Gallery" />
+      </div>
+      <div className="mx-12 my-3 grid grid-cols-1 sm:grid-cols-2 gap-6">
+        {images.map((src, index) => (
+          <div key={index} className="relative w-full h-[350px] overflow-hidden rounded-lg shadow-lg">
+            <Image 
+              src={`/photos/${src}`} 
+              alt={`Image ${index + 1}`} 
+              fill 
+              className="object-contain" // Keeps aspect ratio for all images
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+
+
